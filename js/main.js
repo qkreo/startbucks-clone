6c08873ef5334f2d0626515f7fb9ff14c0697d21
qkreo <bdh407@naver.com>
2024-07-24 11:54:35 +0900

    css,script 공통 부분 분리

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-console.log('js');
-
-const searchEl = document.querySelector('.search');
-
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', () => {
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', () => {
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder','통합검색');
-})
-
-searchInputEl.addEventListener('blur', () => {
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder','');
-})
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -151,6 +131,3 @@ spyEls.forEach((spyEl,index) => {
         .addTo(new ScrollMagic.Controller());
 })
 
-const thisYear = document.querySelector('.this-year');
-
-thisYear.textContent = new Date().getFullYear();
